62b348de2cae41ea063957add41907183609fa8a
Simon Carrignon <simon.Carrignon@gmail.com>
2012-07-16 16:08:16 -0400

    ch

--- src/cutTheLastLine.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#this script cut the list line of file in modelOutputFiles
-
-for i in data/output/modelOutputFiles/*.csv ; do
-	j=${i}_new;
-	head -n $((`wc -l "$i" | awk '{print $1}'`-1)) "$i" > "$j";
-	mv "$j" "$i";
-done
