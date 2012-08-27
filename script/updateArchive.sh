9792b2ea6f4e46a5a33829b73fe0e0f2a9dd68e7
Simon Carrignon <simon.Carrignon@gmail.com>
2012-08-27 11:13:49 -0400

    script upade+archive tgz

--- script/updateArchive.sh
+++ script/updateArchive.sh
@@ -2,8 +2,8 @@
 #Un petit script pour updater l'archive publique telechargeable sur le git
 
 cd ../../
-rm pedestrian/pedestrian*.tgz;
-tar czvf pedestrian`date +%Y-%m-%d`.tgz --exclude-from pedestrian/.archexclude pedestrian/;
-mv pedestrian*.tgz pedestrian;
+rm pedestrian/pedestrian-last.tgz;
+tar czvf pedestrian-last.tgz --exclude-from pedestrian/.archexclude pedestrian/;
+mv pedestrian-last.tgz pedestrian;
 cd pedestrian/script
 
